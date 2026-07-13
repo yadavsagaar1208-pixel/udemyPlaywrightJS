3c1cb981b8f2473b5a2e1c63d7302ebc3669dcad
Sagar Yadav <yadavsagaar1208@gmail.com>
2026-07-13 20:56:36 +0530

    first commit

--- playwright.service.config.js
+++ playwright.service.config.js
@@ -22,8 +22,8 @@ export default defineConfig(
     If you're already using other reporters, add them to this array.
     */
     reporter: [
-     ["html", { open: "never" }],
-     ["@azure/playwright/reporter"],
+      ["html", { open: "never" }],
+      ["@azure/playwright/reporter"],
     ],
   }
 );
